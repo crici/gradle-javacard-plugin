8f94bf39c7363153f9219ddccc602e39b679eb66
Christian Behrens <Christian.Behrens@crici.de>
2016-04-21 01:10:08 +0200

    Using wrapper build

--- src/main/groovy/de/crici/gradle/javacard/JavacardPlugin.groovy
+++ src/main/groovy/de/crici/gradle/javacard/JavacardPlugin.groovy
@@ -3,6 +3,9 @@ package de.crici.gradle.javacard
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.model.RuleSource
+import org.gradle.platform.base.ComponentType
+import org.gradle.platform.base.TypeBuilder
+
 
 class JavacardPlugin implements Plugin<Project> {
 	
